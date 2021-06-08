cb07db7d50dc77def172e5cf2410d5266445f8d0
Danil6969 <20danil02@mail.ru>
2021-06-08 23:29:12 +0300

    Update ParamListStandard.java
    Style correction

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/ParamListStandard.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/lang/ParamListStandard.java
@@ -128,8 +128,9 @@ public class ParamListStandard implements ParamList {
 			if (status[grp] < 0) {
 				continue;
 			}
-			if (!takenUpVarnodes.isEmpty() && overlapsTakenUpVarnodes(takenUpVarnodes, element))
+			if (!takenUpVarnodes.isEmpty() && overlapsTakenUpVarnodes(takenUpVarnodes, element)) {
 				continue;		// Prevent the decompiler from "Overlapping input varnodes" error
+			}
 			if ((element.getType() != ParamEntry.TYPE_UNKNOWN) &&
 				(ParamEntry.getMetatype(tp) != element.getType())) {
 				continue;		// Wrong type
