7bb0c399bca9a8e577cd3ce5373c649ff8ecb8ee
Danil6969 <20danil02@mail.ru>
2022-08-22 15:22:02 +0300

    Update funcdata.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/funcdata.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/funcdata.cc
@@ -25,6 +25,7 @@ ElementId ELEM_VARNODES = ElementId("varnodes",119);
 
 const string Funcdata::extractind = "extractind";
 const string Funcdata::insertind = "insertind";
+const string Funcdata::addr = "addr";
 
 /// \param nm is the (base) name of the function
 /// \param scope is Symbol scope associated with the function
