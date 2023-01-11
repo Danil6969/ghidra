da417f9560a04d6f6c2d42bfe7c823da3b5d8b43
Danil6969 <20danil02@mail.ru>
2023-01-11 21:49:17 +0300

    Add files via upload

---
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/INTPTR_TDataType.java
@@ -0,0 +1,56 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ghidra.program.model.data;
+
+public class INTPTR_TDataType extends AbstractIntegerDataType {
+
+	private static final long serialVersionUID = 1L;
+
+	/** A statically defined INTPTR_TDataType instance.*/
+	public final static INTPTR_TDataType dataType = new INTPTR_TDataType();
+
+	public INTPTR_TDataType() {
+		this(null);
+	}
+
+	public INTPTR_TDataType(DataTypeManager dtm) {
+		super("intptr_t", true, dtm);
+	}
+
+	@Override
+	public String getDescription() {
+		return "signed integer memsize-type";
+	}
+
+	@Override
+	public int getLength() {
+		return dataMgr.getDataOrganization().getPointerSize();
+	}
+
+	@Override
+	public UINTPTR_TDataType getOppositeSignednessDataType() {
+		return UINTPTR_TDataType.dataType.clone(getDataTypeManager());
+	}
+
+	@Override
+	public INTPTR_TDataType clone(DataTypeManager dtm) {
+		if (dtm == getDataTypeManager()) {
+			return this;
+		}
+		return new INTPTR_TDataType(dtm);
+	}
+
+}
---
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/UINTPTR_TDataType.java
@@ -0,0 +1,56 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ghidra.program.model.data;
+
+public class UINTPTR_TDataType extends AbstractIntegerDataType {
+
+	private static final long serialVersionUID = 1L;
+
+	/** A statically defined UINTPTR_TDataType instance.*/
+	public final static UINTPTR_TDataType dataType = new UINTPTR_TDataType();
+
+	public UINTPTR_TDataType() {
+		this(null);
+	}
+
+	public UINTPTR_TDataType(DataTypeManager dtm) {
+		super("uintptr_t", false, dtm);
+	}
+
+	@Override
+	public String getDescription() {
+		return "unsigned integer memsize-type";
+	}
+
+	@Override
+	public int getLength() {
+		return dataMgr.getDataOrganization().getPointerSize();
+	}
+
+	@Override
+	public INTPTR_TDataType getOppositeSignednessDataType() {
+		return INTPTR_TDataType.dataType.clone(getDataTypeManager());
+	}
+
+	@Override
+	public UINTPTR_TDataType clone(DataTypeManager dtm) {
+		if (dtm == getDataTypeManager()) {
+			return this;
+		}
+		return new UINTPTR_TDataType(dtm);
+	}
+
+}
