2fb5621b2d79318ae0c2f09a7f05a62a12c47112
Danil6969 <20danil02@mail.ru>
2023-10-21 23:14:29 +0300

    Update heritage.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/heritage.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/heritage.cc
@@ -1447,23 +1447,8 @@ void Heritage::guardCalls(uint4 fl,const Address &addr,int4 size,vector<Varnode 
     if (fc->isInputActive() && tryregister) {
       int4 inputCharacter = fc->characterizeAsInputParam(transAddr,size);
       if (inputCharacter == ParamEntry::contains_justified) {	// Call could be using this range as an input parameter
-	// Check if fully written in case no guard is required at all
-        bool needsGuard = true;
-        FuncProto &proto = fd->getFuncProto();
-        int4 num = proto.numParams();
-        for (int4 i=0;i<num;++i) {
-          ProtoParameter *param = proto.getParam(i);
-          Address paramAddr = param->getAddress();
-          int4 paramSize = param->getSize();
-          if (paramAddr==transAddr) {
-            if (paramSize>=size) {
-              needsGuard = false; // Already defined at the start of the function
-            }
-          }
-        }
-
-        ParamActive *active = fc->getActiveInput();
-	if (needsGuard&&active->whichTrial(transAddr,size)<0) { // If not already a trial
+	ParamActive *active = fc->getActiveInput();
+	if (active->whichTrial(transAddr,size)<0) { // If not already a trial
 	  PcodeOp *op = fc->getOp();
 	  active->registerTrial(transAddr,size);
 	  Varnode *vn = fd->newVarnode(size,addr);
