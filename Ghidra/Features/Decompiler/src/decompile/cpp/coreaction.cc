fa7b0fd8fb670a76ef40222d669ce897adb87c43
Danil6969 <20danil02@mail.ru>
2023-11-08 21:30:26 +0300

    Update coreaction.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -5740,6 +5740,7 @@ void ActionDatabase::universalAction(Architecture *conf)
       {
 	actprop2 = new ActionPool(Action::rule_repeatapply,"oppool2");
 
+	actprop2->addRule( new RuleUnlinkPtrAdd("typerecovery") );
 	actprop2->addRule( new RulePushPtr("typerecovery") );
 	actprop2->addRule( new RuleStructOffset0("typerecovery") );
 	actprop2->addRule( new RulePtrArith("typerecovery") );
