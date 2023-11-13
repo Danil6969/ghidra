45b9150d6730c61af24f3376efcbeee765d7ea7b
Danil6969 <20danil02@mail.ru>
2023-11-13 18:07:52 +0300

    Update ghidraRun.bat

--- Ghidra/RuntimeScripts/Windows/ghidraRun.bat
+++ Ghidra/RuntimeScripts/Windows/ghidraRun.bat
@@ -5,7 +5,7 @@ setlocal
 
 :: Maximum heap memory may be changed if default is inadequate. This will generally be up to 1/4 of 
 :: the physical memory available to the OS. Uncomment MAXMEM setting if non-default value is needed.
-::set MAXMEM=2G
+set MAXMEM=2G
 
 call "%~dp0support\launch.bat" bg jdk Ghidra "%MAXMEM%" "" ghidra.GhidraRun %*
 
