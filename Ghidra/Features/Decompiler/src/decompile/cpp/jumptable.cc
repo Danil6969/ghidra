26bd82641dd185ade98657fbbe3f40406ed91ab1
Danil6969 <20danil02@mail.ru>
2023-12-24 17:37:55 +0300

    Update jumptable.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/jumptable.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/jumptable.cc
@@ -431,8 +431,7 @@ bool JumpBasic::isprune(Varnode *vn)
 {
   if (!vn->isWritten()) return true;
   PcodeOp *op = vn->getDef();
-  if (op->isCall()) return true;
-  if (op->isMarker()) return true;
+  if (op->isCall()||op->isMarker()) return true;
   if (op->numInput()==0) return true;
   return false;
 }
