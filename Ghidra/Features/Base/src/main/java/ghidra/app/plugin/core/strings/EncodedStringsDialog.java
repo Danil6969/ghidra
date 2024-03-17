982da26ef946c0248cfe725517cc272258f7dab3
Danil6969 <20danil02@mail.ru>
2024-03-17 21:30:27 +0300

    Update EncodedStringsDialog.java

--- Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/strings/EncodedStringsDialog.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/plugin/core/strings/EncodedStringsDialog.java
@@ -365,7 +365,7 @@ public class EncodedStringsDialog extends DialogComponentProvider {
 		tableModel = new EncodedStringsTableModel(program, selectedAddresses);
 		tableModel.addTableModelListener(e -> {
 			Integer rowNum = rowToSelect.getAndSet(null);
-			if (rowNum != null && table.getRowCount() > 0) {
+			if (rowNum != null && table.getRowCount() > rowNum) {
 				table.selectRow(rowNum);
 				table.requestFocusInWindow();
 			}
