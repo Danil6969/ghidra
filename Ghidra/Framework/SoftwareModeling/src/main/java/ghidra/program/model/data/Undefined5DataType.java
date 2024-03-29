07bc5f8d5d877d96a02e4ab21f2f40d063adc40c
Danil6969 <20danil02@mail.ru>
2024-03-29 03:02:18 +0300

    Update Undefined5DataType.java

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/Undefined5DataType.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/Undefined5DataType.java
@@ -88,7 +88,8 @@ public class Undefined5DataType extends Undefined {
 		try {
 			long b = getValue(buf);
 			val = Long.toHexString(b).toUpperCase();
-			val = StringFormat.padIt(val, 10, 'h', true);
+			val = StringFormat.padIt(val, 10, '\0', true);
+			val = "0x" + val;
 		}
 		catch (MemoryAccessException e) {
 		}
