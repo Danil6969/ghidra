c7dfd4d83188c94cd6d1acaec15c4e27e9ce0605
Danil6969 <20danil02@mail.ru>
2024-06-18 14:49:56 +0300

    Update varnode.hh

--- Ghidra/Features/Decompiler/src/decompile/cpp/varnode.hh
+++ Ghidra/Features/Decompiler/src/decompile/cpp/varnode.hh
@@ -349,6 +349,7 @@ public:
   const PcodeOp *getAllocaShiftOp(Funcdata &data) const;
   bool isAllocaAddress(Funcdata &data) const { return getAllocaShiftOp(data) != (PcodeOp *)0; }	///< Does \b this varnode have corresponding alloca shift op
   bool isAllocaLength(Funcdata &data) const;
+  bool isStackPointerLocated(Funcdata &data) const;
   bool isStackVariableAddress(Funcdata &data) const;
   bool isPtrdiffSubtrahend(Funcdata &data) const;
 };
