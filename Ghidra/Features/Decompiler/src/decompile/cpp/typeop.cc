27b44cff12e33521c33ec9bd6483074a68853844
Danil6969 <20danil02@mail.ru>
2024-07-24 14:31:35 +0300

    Update typeop.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/typeop.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/typeop.cc
@@ -1456,6 +1456,10 @@ Datatype *TypeOpIntSub::getOutputLocal(const PcodeOp *op) const
 Datatype *TypeOpIntSub::getOutputToken(const PcodeOp *op,CastStrategy *castStrategy) const
 
 {
+  if (op->isAllocaShift()) {
+    AddrSpace *spc = tlst->getArch()->getDefaultDataSpace();
+    return tlst->getTypePointer(op->getOut()->getSize(),tlst->getBase(1,TYPE_VOID),spc->getWordSize());
+  }
   return castStrategy->arithmeticOutputStandard(op);	// Use arithmetic typing rules
 }
 
