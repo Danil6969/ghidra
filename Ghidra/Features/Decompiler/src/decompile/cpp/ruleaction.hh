51178657fe0c00425d4b491b260b3393d0093e1e
Danil6969 <20danil02@mail.ru>
2024-08-21 23:53:07 +0300

    Update ruleaction.hh

--- Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.hh
+++ Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.hh
@@ -1069,6 +1069,17 @@ public:
   virtual void getOpList(vector<uint4> &oplist) const;
   virtual int4 applyOp(PcodeOp *op,Funcdata &data);
 };
+class RuleAllocaPushParams : public Rule {
+  static PcodeOp *getCorrespondingLoadOp(PcodeOp *storeop);
+public:
+  RuleAllocaPushParams(const string &g) : Rule(g, 0 , "allocapushparams") {}	///< Constructor
+  virtual Rule *clone(const ActionGroupList &grouplist) const {
+    if (!grouplist.contains(getGroup())) return (Rule *)0;
+    return new RuleAllocaPushParams(getGroup());
+  }
+  virtual void getOpList(vector<uint4> &oplist) const;
+  virtual int4 applyOp(PcodeOp *op,Funcdata &data);
+};
 class RuleCancelOutPtrAdd : public Rule {
   static void gatherNegateOps(PcodeOp *op,vector<PcodeOp *> &negateops);
   static void gatherPossiblePairingOps(Varnode *op,vector<PcodeOp *> &multis,vector<Varnode *> &others);
