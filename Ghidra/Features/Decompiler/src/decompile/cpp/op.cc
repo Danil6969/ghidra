782a5754f8c695d856f7a1350c7bf52ded9b3c7f
Danil6969 <20danil02@mail.ru>
2024-11-17 11:00:49 +0300

    Update op.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/op.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/op.cc
@@ -85,8 +85,10 @@ int4 PcodeOp::getPointerSlot(void) const
 
 {
   OpCode opc = code();
+  if (opc == CPUI_COPY) return 0;
   if (opc == CPUI_LOAD) return 1;
   if (opc == CPUI_STORE) return 1;
+  if (opc == CPUI_CAST) return 0;
   if (opc == CPUI_PTRADD) return 0;
   if (opc == CPUI_PTRSUB) return 0;
   return -1;
