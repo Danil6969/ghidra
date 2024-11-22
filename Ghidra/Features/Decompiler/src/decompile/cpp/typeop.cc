0cdaac53d717609a2d0f61ac2666f518b3b49643
Danil6969 <20danil02@mail.ru>
2024-11-23 01:08:37 +0300

    Update typeop.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/typeop.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/typeop.cc
@@ -1376,7 +1376,9 @@ Datatype *TypeOpIntAdd::getInputLocal(const PcodeOp *op,int4 slot) const
   const Varnode *invn = op->getIn(slot);
   const Varnode *outvn = op->getOut();
 
-  if (invn->getTypeReadFacing(op)->getMetatype() == TYPE_PTR || outvn->getTypeDefFacing()->getMetatype() == TYPE_PTR)
+  if (invn->getTypeReadFacing(op)->getMetatype() == TYPE_PTR)
+    return tlst->getMemsizeType(true);
+  if (outvn->getTypeDefFacing()->getMetatype() == TYPE_PTR)
     return tlst->getMemsizeType(true);
   return tlst->getBaseNoChar(op->getIn(slot)->getSize(),TYPE_INT);
 }
