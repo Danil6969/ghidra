e748ed9c4c6281e8b8e47c205fa5fa3668b70607
Danil6969 <20danil02@mail.ru>
2025-01-14 23:49:25 +0300

    Create Undefined11DataType.java

---
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/Undefined11DataType.java
@@ -0,0 +1,118 @@
+/* ###
+ * IP: GHIDRA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ghidra.program.model.data;
+
+import java.math.BigInteger;
+
+import ghidra.docking.settings.Settings;
+import ghidra.program.model.mem.MemBuffer;
+import ghidra.util.DataConverter;
+import ghidra.util.StringFormat;
+import ghidra.util.classfinder.*;
+
+/**
+ * Provides an implementation of an 11-byte that has not been defined yet as a
+ * particular type of data in the program.
+ */
+public class Undefined11DataType extends Undefined {
+	static {
+		ClassTranslator.put("ghidra.program.model.data.Undefined12",
+			Undefined11DataType.class.getName());
+	}
+
+	private final static long serialVersionUID = 1;
+	private static final EndianSettingsDefinition ENDIAN = EndianSettingsDefinition.DEF;
+
+	/** A statically defined DefaultDataType used when an Undefined byte is needed.*/
+	public final static Undefined11DataType dataType = new Undefined11DataType();
+
+	/**
+	 * Constructs a new Undefined11 dataType
+	 *
+	 */
+	public Undefined11DataType() {
+		this(null);
+	}
+
+	public Undefined11DataType(DataTypeManager dtm) {
+		super("undefined11", dtm);
+	}
+
+	/**
+	 * 
+	 * @see ghidra.program.model.data.DataType#getLength()
+	 */
+	public int getLength() {
+		return 11;
+	}
+
+	/**
+	 * 
+	 * @see ghidra.program.model.data.DataType#getDescription()
+	 */
+	public String getDescription() {
+		return "Undefined 11-Byte";
+	}
+
+	/**
+	 * 
+	 * @see ghidra.program.model.data.DataType#getMnemonic(Settings)
+	 */
+	public String getMnemonic(Settings settings) {
+		return name;
+	}
+
+	/**
+	 * 
+	 * @see ghidra.program.model.data.DataType#getRepresentation(MemBuffer, Settings, int)
+	 */
+	public String getRepresentation(MemBuffer buf, Settings settings, int length) {
+		String val = "??";
+
+		Object b = getValue(buf, settings, length);
+		if (!(b instanceof BigInteger)) {
+			return val;
+		}
+		BigInteger bi = (BigInteger)b;
+		val = bi.toString(16).toUpperCase();
+		val = StringFormat.padIt(val, 22, '\0', true);
+		val = "0x" + val;
+
+		return val;
+	}
+
+	/**
+	 * 
+	 * @see ghidra.program.model.data.DataType#getValue(ghidra.program.model.mem.MemBuffer, ghidra.docking.settings.Settings, int)
+	 */
+	public Object getValue(MemBuffer buf, Settings settings, int length) {
+		byte[] bytes = new byte[11];
+		if (buf.getBytes(bytes, 0) != 11) {
+			return null;
+		}
+
+		DataConverter dc = DataConverter.getInstance(ENDIAN.isBigEndian(settings, buf));
+
+		return dc.getBigInteger(bytes, 11, false);
+	}
+
+	public DataType clone(DataTypeManager dtm) {
+		if (dtm == getDataTypeManager()) {
+			return this;
+		}
+		return new Undefined11DataType(dtm);
+	}
+}
