1a37cd1011771c1fe211337b35f2fdf8d72c4ad3
Danil6969 <20danil02@mail.ru>
2025-01-24 04:47:47 +0300

    Update coreaction.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/coreaction.cc
@@ -3466,8 +3466,8 @@ int4 ActionMarkExplicit::baseExplicit(Varnode *vn,int4 maxref)
 
   PcodeOp *def = vn->getDef();
   if (def == (PcodeOp *)0) return -1;
-  Funcdata &data = *def->getFuncdata();
-  if (vn->isAllocaAddress(data)) return -1;
+  Funcdata *data = def->getFuncdata();
+  if (vn->isAllocaAddress(*data)) return -1;
   if (def->isMarker()) return -1;
   if (def->isCall()) {
     if ((def->code() == CPUI_NEW)&&(def->numInput() == 1))
