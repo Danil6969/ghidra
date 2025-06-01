7863fad64e9f30082b2cba5991b8a6fec769ebbd
Danil6969 <20danil02@mail.ru>
2025-06-01 16:42:52 +0300

    Update printc.hh

--- Ghidra/Features/Decompiler/src/decompile/cpp/printc.hh
+++ Ghidra/Features/Decompiler/src/decompile/cpp/printc.hh
@@ -180,9 +180,9 @@ protected:
   bool checkArrayDeref(const Varnode *vn) const;	///< Determine whether a LOAD/STORE expression requires pointer '*' syntax
   bool needsToArr(const Varnode *vn) const;		///< Determine whether TOARR is required because of rvalue restrictions
   bool checkAddressOfCast(const PcodeOp *op) const;	///< Check if CAST can be printed as an '&'
-  bool isNonstructCast(Datatype *inType,Datatype *outType) const;
-  bool isUpcast(Datatype *inType,Datatype *outType) const;
-  bool isSimpleCast(Datatype *inType,Datatype *outType) const;
+  bool isNonstructCast(Datatype *inType,Datatype *outType,TypeFactory *types) const;
+  bool isClassUpcast(Datatype *inType,Datatype *outType,TypeFactory *types) const;
+  bool isSimpleCast(Datatype *inType,Datatype *outType,TypeFactory *types) const;
   void emitStructDefinition(const TypeStruct *ct);	///< Emit the definition of a \e structure data-type
   void emitEnumDefinition(const TypeEnum *ct);		///< Emit the definition of an \e enumeration data-type
   void emitPrototypeOutput(const FuncProto *proto,const Funcdata *fd);	///< Emit the output data-type of a function prototype
