05853de0fec88716a1666c17db581a3848ae4dae
Danil6969 <20danil02@mail.ru>
2025-09-11 15:05:56 +0300

    Update ruleaction.cc

--- Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.cc
+++ Ghidra/Features/Decompiler/src/decompile/cpp/ruleaction.cc
@@ -3793,7 +3793,8 @@ int4 RulePropagateCopy::applyOp(PcodeOp *op,Funcdata &data)
     copyop = vn->getDef();
     if (copyop->code()!=CPUI_COPY)
       continue;			// not a propagating instruction
-    
+    if (copyop->isStaticCastCopy(data)) continue;
+
     invn = copyop->getIn(0);
     if (!invn->isHeritageKnown()) continue; // Don't propagate free's away from their first use
     if (invn == vn)
