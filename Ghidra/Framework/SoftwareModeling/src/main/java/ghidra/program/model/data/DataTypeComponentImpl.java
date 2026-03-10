b8a54571b0433becca198bdb2c994888a25e53e6
Danil6969 <20danil02@mail.ru>
2026-03-10 23:46:34 +0300

    Update DataTypeComponentImpl.java

--- Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/DataTypeComponentImpl.java
+++ Ghidra/Framework/SoftwareModeling/src/main/java/ghidra/program/model/data/DataTypeComponentImpl.java
@@ -32,7 +32,7 @@ public class DataTypeComponentImpl implements InternalDataTypeComponent, Seriali
 	private final static long serialVersionUID = 1;
 
 	private DataType dataType;
-	private Composite parent; // parent prototype containing us
+	private DataType parent; // parent prototype containing us
 	private int offset; // offset in parent
 	private int ordinal; // position in parent
 	private SettingsImpl defaultSettings;
@@ -51,7 +51,7 @@ public class DataTypeComponentImpl implements InternalDataTypeComponent, Seriali
 	 * @param fieldName the name associated with this component
 	 * @param comment the comment associated with this component
 	 */
-	public DataTypeComponentImpl(DataType dataType, Composite parent, int length,
+	public DataTypeComponentImpl(DataType dataType, DataType parent, int length,
 			int ordinal, int offset, String fieldName, String comment) {
 
 		this.parent = parent;
@@ -71,7 +71,7 @@ public class DataTypeComponentImpl implements InternalDataTypeComponent, Seriali
 	 * @param ordinal the index of this component within its parent.
 	 * @param offset the byte offset within the parent
 	 */
-	public DataTypeComponentImpl(DataType dataType, Composite parent, int length,
+	public DataTypeComponentImpl(DataType dataType, DataType parent, int length,
 			int ordinal, int offset) {
 		this(dataType, parent, length, ordinal, offset, null, null);
 	}
@@ -138,9 +138,12 @@ public class DataTypeComponentImpl implements InternalDataTypeComponent, Seriali
 		if (parent == null) {
 			return; // Bad situation
 		}
-		for (DataTypeComponent comp : parent.getDefinedComponents()) {
-			if (comp != this && name.equals(comp.getFieldName())) {
-				throw new DuplicateNameException("Duplicate field name: " + name);
+		if (parent instanceof Composite) {
+			Composite composite = (Composite) parent;
+			for (DataTypeComponent comp : composite.getDefinedComponents()) {
+				if (comp != this && name.equals(comp.getFieldName())) {
+					throw new DuplicateNameException("Duplicate field name: " + name);
+				}
 			}
 		}
 	}
